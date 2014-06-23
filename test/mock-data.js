8fc82ecb5f70b45a4e6f5389c42d6579da90a70a
Kate Hudson <k88hudson@gmail.com>
2014-06-23 15:50:07 -0400

    Updated test data

--- test/mock-data.js
+++ test/mock-data.js
@@ -10,8 +10,7 @@ module.exports = {
       eventUrl: "https://events.webmaker.org/#!/events/3452",
       confirmUrlYes: "https://events.webmaker.org/confirm/somerandomtoken&confirmation=yes",
       confirmUrlNo: "https://events.webmaker.org/confirm/somerandomtoken&confirmation=no",
-      organizerUsername: "k88hudson",
-      comments: "Great job!!"
+      organizerUsername: "k88hudson"
     },
     // This is if the user does not exist
     {
@@ -24,14 +23,15 @@ module.exports = {
   ],
   hive_badge_awarded: [
     {
-      "username": "k88hudson",
-      "badgeUrl": "https://webmaker.org/badges/hive-community-member",
-      "profileUrl": "https://webmaker.org/user/k88hudson"
+      username: "k88hudson",
+      badgeUrl: "https://webmaker.org/badges/hive-community-member",
+      profileUrl: "https://webmaker.org/user/k88hudson",
+      comments: "Great job!!"
     },
     // No username
     {
-      "badgeUrl": "https://webmaker.org/badges/hive-community-member",
-      "signUpUrl": "https://webmaker.org"
+      badgeUrl: "https://webmaker.org/badges/hive-community-member",
+      signUpUrl: "https://webmaker.org"
     }
   ]
 };
