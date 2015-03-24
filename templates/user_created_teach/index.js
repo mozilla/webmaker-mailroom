cfe2b3c7c0c844e961d53e0eddca5a386f825fc0
Christopher De Cairos <chris@chrisdecairos.ca>
2015-03-24 14:31:19 -0400

    Welcome new users referred from teach site

---
+++ templates/user_created_teach/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'New user created - Teach',
+  description: 'This is sent to a user when they first create a Webmaker account from the teach site',
+  subject: 'Welcome to the Mozilla Teach the Web Community!',
+  tests: [{
+    description: 'User jon created an account',
+    data: {}
+  }]
+};
