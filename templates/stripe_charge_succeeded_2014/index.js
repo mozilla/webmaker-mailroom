cb2c4c227c63591362a25ef5e80941e41fa10e2f
Jon Buckley <jon@jbuckley.ca>
2015-11-17 09:06:39 -0500

    Update translation for es locale

--- templates/stripe_charge_succeeded_2014/index.js
+++ templates/stripe_charge_succeeded_2014/index.js
@@ -30,6 +30,7 @@ module.exports = {
       data: {
         name: 'es donor',
         amount: '$10',
+        transaction_id: 'ch_fakeid',
         address: '366 Adelaide St W',
         city: 'Toronto',
         state: 'ON',
