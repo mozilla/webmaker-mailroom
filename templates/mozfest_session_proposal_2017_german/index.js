b7f6fd3580d16bc9daa6eaa4b4e2ac3493c92056
Mavis Ou <mmmavis@users.noreply.github.com>
2017-06-08 20:38:11 -0700

    Fixes #60 - added templates for MozFest German and French proposal confirmation email

--- templates/mozfest_session_proposal_2017_german/index.js
+++ templates/mozfest_session_proposal_2017_german/index.js
@@ -1,22 +1,22 @@
 module.exports = {
   // Choose a human-readable name. This is just for rendering example pages.
-  name: '2017 Mozilla Festival Session Proposal (English)',
+  name: '2017 Mozilla Festival Session Proposal (German)',
 
   // Choose a good description; when is this email sent?
-  description: 'When someone submits a session proposal on the Mozilla Festival website',
+  description: 'When someone submits a session proposal in German on the Mozilla Festival website',
 
   // Choose a subject for your email
-  subject: 'Your MozFest session proposal',
+  subject: 'Ihr Vorschlag für eine MozFest-Veranstaltung',
 
   // Create a test case for every possible version of the email,
   // E.g. with a user name, without a username
   tests: [
     {
-      description: 'Chris submits a proposal',
+      description: 'Chris submits a proposal in German',
       data: {
         first_name: 'Chris',
         github_issue_url: "https://github.com/MozillaFoundation/mozfest-program-2017/issues/1",
-        github_issue_title: "How to Devops like a Boss"
+        github_issue_title: "Taten sagen mehr als Worte"
       }
     }
   ]
